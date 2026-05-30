bf4385caf78cd8499b25a73737032db366d431d5
c4rlosantonio527 <c4rlosantonio527@gmail.com>
2026-05-30 10:59:07 -0300

    feat(posts): implementar feed y configuracion inicial de publicaciones

--- posts/views.py
+++ posts/views.py
@@ -1,34 +1,6 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from .models import Post
 
-
-@login_required
 def feed(request):
-    posts = Post.objects.select_related('author').all()
-
-    return render(
-        request,
-        'posts/feed.html',
-        {'posts': posts}
-    )
-
-
-@login_required
-def create_post(request):
-
-    if request.method == 'POST':
-
-        content = request.POST.get('content')
-
-        if content:
-
-            Post.objects.create(
-                author=request.user,
-                content=content
-            )
-
-        return redirect('feed')
-
-    return redirect('feed')
-#Añadido 7 c4
+    posts = Post.objects.all().order_by("-created_at")
+    return render(request, "posts/feed.html", {"posts": posts})
