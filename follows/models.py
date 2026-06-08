09421c0164c6da576f0d69f327b7b1499df46c65
gustavoantonich <gustavoantonich187@gmail.com>
2026-06-08 20:11:07 -0300

    feat(follows): implementar modelo Follow con migracion y registro en admin

--- follows/models.py
+++ follows/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+    following = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='followers'
+    )
+
+    class Meta:
+        unique_together = ('follower', 'following')
+
+    def __str__(self):
+        return f'{self.follower} -> {self.following}'
