b140d42501f2962dc349f72e1f152a7799ca45d0
c4rlosantonio527 <c4rlosantonio527@gmail.com>
2026-06-10 20:28:05 -0300

    feat(media): integrar Cloudinary para almacenamiento de imagenes en la nube

--- config/settings.py
+++ config/settings.py
@@ -138,10 +138,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+#Cambiar luego
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': os.environ.get('dv08wuls3'),
+    'API_KEY': os.environ.get('277557232732656'),
+    'API_SECRET': os.environ.get('vNWFaLfzSTu4ErU9Baf1CBSjouw'),
 }
 
 # Default primary key field type
--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
 
-    path('profile/<str:username>/', views.profile_view, name='profile'),
     path('profile/edit/', views.edit_profile_view, name='edit_profile'),
+    path('profile/<str:username>/', views.profile_view, name='profile'),
 ]
